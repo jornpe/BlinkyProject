1f7daea6e0571167bafa34a6a88c5d7783fede15
Jørn Pettersen <joern.pettersen@hotmail.com>
2022-07-01 07:20:04 +0200

    🎉 Added new blazor based web application

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,7 +1,21 @@
+using Azure.Identity;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Azure.Devices;
+using WebApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddScoped(provider =>
+{
+    var token = new DefaultAzureCredential();
+    return RegistryManager.Create("", token);
+});
 
 var app = builder.Build();
 
@@ -14,12 +28,12 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
