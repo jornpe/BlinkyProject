7d80f9aae3203b7fa0e11586a0201f2490d34406
Jørn Pettersen <joern.pettersen@hotmail.com>
2022-07-11 09:29:24 +0200

    ✨ Using app config in azure

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -3,15 +3,38 @@ using Microsoft.Azure.Devices;
 using WebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var token = new DefaultAzureCredential();
 
-ConfigureIothubConnection(builder);
+if (!Uri.TryCreate( builder.Configuration.GetValue<string>("AppConfig:Endpoint"), UriKind.Absolute, out var endpoint))
+{
+    throw new InvalidOperationException("App configuration URI is not valid");
+}
+
+
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    options.Connect(endpoint, token)
+           .Select("Blinkey:*");
+});
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddAzureAppConfiguration();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped<DeviceTwinService>();
 builder.Services.AddScoped<IotDevicesService>();
 
+var IothubHostName = builder.Configuration.GetValue<string>("Blinkey:IotHubOptions:HostName");
+
+builder.Services.AddScoped(provider =>
+{
+    return ServiceClient.Create(IothubHostName, token);
+});
+
+builder.Services.AddScoped(provider =>
+{
+    return RegistryManager.Create(IothubHostName, token);
+});
 
 var app = builder.Build();
 
@@ -33,21 +56,3 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
-
-
-
-static void ConfigureIothubConnection(WebApplicationBuilder builder)
-{
-    var IothubHostName = builder.Configuration.GetValue<string>("IotHubOptions:HostName");
-    builder.Services.AddScoped(provider =>
-    {
-        var token = new DefaultAzureCredential();
-        return ServiceClient.Create(IothubHostName, token);
-    });
-
-    builder.Services.AddScoped(provider =>
-    {
-        var token = new DefaultAzureCredential();
-        return RegistryManager.Create(IothubHostName, token);
-    });
-}
